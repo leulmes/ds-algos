2b16933a508ae3efbfe84ec7ce801b71dd089902
Leul Mesfin <leulmesfin152025@gmail.com>
2025-01-05 11:40:09 -0500

    inefficient bubble sort

--- sorting-algos/bubble_sort.py
+++ sorting-algos/bubble_sort.py
@@ -6,12 +6,16 @@ def swap(lst, x, y):
 def bubble_sort(nums):
     N = len(nums)
     for i in range(N):
-        for j in range(i + 1, N):
-            if nums[j] < nums[i]:
-                # swap!
-                swap(nums, i, j)
+        for j in range(0, N - 1):
+            if nums[j] > nums[j + 1]:
+                print(f'{nums[j]} is > than {nums[j + 1]}')
+                print(f'swapping {nums[j]} and {nums[j + 1]}')
+                print('list BEFORE swap: ', nums)
+                swap(nums, j, j + 1)
+                print('list AFTER swap: ', nums)
     return nums
-
+    
+# print(bubble_sort([2, 4, 1, 5, 3, 7]))
 print(bubble_sort([8, 4, 3, 4, 1]))
 
 # Time: O(n^2) -> n iterations for outer loop in worst case, n - 1 iterations for inner loop in worst case
