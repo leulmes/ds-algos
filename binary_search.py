b3584995caa9c53daefb2f383d8636fc943d03a6
Leul Mesfin <leulmesfin152025@gmail.com>
2025-02-08 10:27:26 -0500

    added sum comments

--- binary_search.py
+++ binary_search.py
@@ -1,16 +1,20 @@
 
+#  Binary Search algorithm
+#  This function returns the target and returns -1, if the target is not found.
+#  Divide & Conquer algo
 def binary_search(lst, target):
     if len(lst) == 0:
         return -1
     
     N = len(lst)
     l, r = 0, N - 1
-    lst.sort() # sort a list
+    lst.sort() # Binary Search works only on a sorted list
 
     print('the left: ', l)
     print('the right: ', r)
     print('mid: ', (r - l) // 2)
     
+    # we use <= instead of < to handle the casee of a 1 elt list. See example below...
     while l <= r:
         mid = (l + r) // 2
         print('mid: ', mid)
@@ -27,4 +31,5 @@ def binary_search(lst, target):
     return -1 # target not found
 
 print(binary_search([0, 0, 4, 4, 6, 7, 8, 9, 9, 10, 12, 13, 13, 14, 14, 17, 18, 19, 19, 19], 17))
+print(binary_search([1], 1)) # 1 element list example
     
