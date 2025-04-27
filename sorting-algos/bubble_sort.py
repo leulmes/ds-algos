7c447054f0ccb36234de6c84dd6f070414d71cfd
Leul Mesfin <leulmesfin152025@gmail.com>
2025-04-27 07:02:52 -0400

    updated bubble sort comment

--- sorting-algos/bubble_sort.py
+++ sorting-algos/bubble_sort.py
@@ -4,14 +4,14 @@ def swap(lst, idx1, idx2):
 
 def bubble_sort(lst):
     N = len(lst)
-    flag = False
+    flag = False 
 
     for i in range(N): # num of sorted elts
         for j in range(N - i - 1):
             if lst[j] > lst[j + 1]:
                 swap(lst, j, j + 1)
                 flag = True
-        if not flag:
+        if not flag: # optimization for best case
             break
 
 
